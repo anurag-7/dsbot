fbc66ec07c7bd8b24221c79333f02d6d3ededd96
Anurag Singh <anurag.singh654@gmail.com>
2021-09-12 02:24:23 +0530

    use rust bindings

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .datesolver import *
--- cogs/date_solver.py
+++ cogs/date_solver.py
@@ -2,12 +2,15 @@ import asyncio
 import math
 import os
 from copy import deepcopy
-
+import discord
 import cv2
 import numpy as np
 from discord.ext import commands
 import aiohttp
 import functools
+from discord.ext.commands.cooldowns import BucketType
+
+from .datesolver import game
 
 default_maze = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                 [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
@@ -25,34 +28,51 @@ default_maze = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                 [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
-places = {
-    "home": {},
-    "shopping": {},
-    "gas": {"gas": 100},
-    "flower": {"mood": 100},
-    "taco": {"food": 60},
-    "ballroom": {"mood": 100, "drink": -15, "food": -15},
-    "coffee": {"drink": 60},
-    "juice": {"drink": 60},
-    "theater": {"mood": 60},
-    "restaurant": {"food": 60},
-    "nightclub": {"drink": 40, "mood": 40},
-    "fair": {"mood": 40, "food": 20, "drink": 20},
-    "sandwhich": {"food": 40, "drink": 20}
-}
+translate = {
+    "tree": 0,
+    "airport": 0,
+    "gas": 15,
 
-moveset = {
-    'UP': [(-2, 0, 'UP'), (-1, -1, 'LEFT'), (-1, 1, 'RIGHT')],
-    'DOWN': [(2, 0, 'DOWN'), (1, 1, 'RIGHT'), (1, -1, 'LEFT')],
-    'RIGHT': [(-1, 1, 'UP'), (0, 2, 'RIGHT'), (1, 1, 'DOWN')],
-    'LEFT': [(-1, -1, 'UP'), (0, -2, 'LEFT'), (1, -1, 'DOWN')]
-}
+    "taco": 25,
+    "ballroom": 30,
+    "coffee": 35,
+    "juice": 40,
+    "theater": 45,
+    "restaurant": 50,
+    "nightclub": 55,
+    "fair": 60,
+    "sandwhich": 65,
 
-interaction_set = {
-    'UP': [(0, -1), (0, 1)],
-    'DOWN': [(0, -1), (0, 1)],
-    'RIGHT': [(1, 0), (-1, 0)],
-    'LEFT': [(1, 0), (-1, 0)]
+    "home": 105,
+    "shopping": 110,
+    "ring": 115,
+    "flower": 120,
+
+    "up": 0,
+    "down": 1,
+    "left": 2,
+    "right": 3,
+
+    15: "GAS",
+    25: "TACO",
+    30: "BALLROOM",
+    35: "COFFEE",
+    40: "JUICE",
+    45: "THEATER",
+    50: "RESTAURANT",
+    55: "NIGHTCLUB",
+    60: "FAIR",
+    65: "SANDWHICH",
+
+    105: "HOME",
+    110: "SHOPPING",
+    115: "RING",
+    120: "FLOWER",
+
+    0: "UP",
+    1: "DOWN",
+    2: "LEFT",
+    3: "RIGHT"
 }
 
 def pretty_maze(maze):
@@ -73,19 +93,15 @@ class DateSolver(commands.Cog):
 
     def match_template(self, image):
 
-        result = ''
-
+        result = 0
+        ring = False
         for name, t in self.templates.items():
             res = cv2.matchTemplate(image, t, cv2.TM_CCORR_NORMED)
             if np.amax(res) >= 0.99:
-                result = name
+                result = int(translate[name])
+                ring = True if result == 115 else ring
                 break
-        return result
-
-
-    def apply_int(self, int_type, **kwargs):
-        res = {k: min(100, v + places[int_type].get(k, 0)) for k, v in kwargs.items()}
-        return res.values()
+        return result, ring
 
     def make_map(self, image):
         maze = deepcopy(default_maze)
@@ -122,14 +138,15 @@ class DateSolver(commands.Cog):
         for i, k in zip(range(1, 15, 2), y_checks):
             for j, coords in zip(range(2, 11, 2), k):
                 maze[i][j] = int(sum(image[coords[1], coords[0]]) == 252)
-
+        ring = False
         for i, (x, y, diff) in zip(range(1, 14, 2), source):
             for j in range(1, 10, 2):
-                maze[i][j] = self.match_template(image[y: y + dim[1], x: x + dim[0]])  # type: ignore
+                maze[i][j], r = self.match_template(image[y: y + dim[1], x: x + dim[0]])  # type: ignore
+                ring = True if r else ring
                 x += diff
 
         ori = 'LEFT' if sum(image[573, 389]) == 523 else 'RIGHT'
-        return maze, ori
+        return maze, ori, ring
 
     async def fetch_image(self, url):
         async with aiohttp.ClientSession() as session:
@@ -138,95 +155,55 @@ class DateSolver(commands.Cog):
                 image = cv2.imdecode(b, cv2.IMREAD_COLOR)
                 return image
 
-    def game(self, image):
-        maze, base_ori = self.make_map(image)
-        best, rpath = -1, None
+    def game_setup(self, maze, base_ori, ring):
 
-        def solve(x, y, ori, gas=100, food=50, drink=50, mood=75, path=[], shopping=False, total=0, bl=dict()):
-            global places, moveset, interaction_set
-            nonlocal best, rpath
-
-            for stat in (food, mood, gas, drink):
-                if stat <= 0:
-                    return
-
-            if total == 25:
-
-                ap = math.ceil((food + drink + mood) / 6) + shopping * 30
-
-                if ap > best:
-                    best = ap
-                    rpath = path
-
-                return
-
-            interactions = [
-                (y + py, x + px) for py, px in interaction_set[ori]
-                if 0 <= y + py < 15 and 0 <= x + px < 11
-            ]
-
-            moves = [
-                (y + my, x + mx, mori) for my, mx, mori in moveset[ori]
-                if 0 <= y + my < 15 and 0 <= x + mx < 11 and maze[y + my][x + mx]
-            ]
-
-            for iy, ix in interactions:
-
-                interaction = maze[iy][ix]
-                if interaction not in places or bl.get((iy, ix), -2) > total - 1:
-                    continue
-
-                new_bl = {}
-                for k, v in bl.items():
-                    new_bl[k] = v
-                if interaction == 'shopping':
-                    new_bl[(iy, ix)] = total+100
-                    solve(x, y, ori, gas=gas, food=food - 4, drink=drink - 6, mood=mood - 8, path=path + [interaction.upper()],
-                        shopping=True, total=total + 1, bl=new_bl)
-
-                elif interaction == 'flower':
-                    new_bl[(iy, ix)] = total+100
-                    g, d, f, m = [min(100, v + places[interaction].get(k, 0)) for k, v in (('gas', gas), ('drink', drink), ('food', food), ('mood', mood))]
-
-                    solve(x, y, ori, gas=g, food=f - 4, drink=d - 6, mood=m - 8, path=path + [interaction.upper()],
-                        shopping=shopping, total=total + 1, bl=new_bl)
-
-                elif interaction == 'home':
-
-                    rem = (25 - len(path)) * 4
-                    ap =  math.ceil((food + drink + mood) / 6 * (1 - rem/100)) + shopping * 30
-                    if ap > best:
-                        best = ap
-                        rpath = path + ["HOME"]
-
-                else:
-                    g, f, m, d = self.apply_int(interaction, gas=gas, food=food, mood=mood, drink=drink)
-                    new_bl[(iy, ix)] = total+10
-                    solve(x, y, ori, gas=g, food=f - 4, drink=d - 6, mood=m - 8, path=path + [interaction.upper()],
-                        shopping=shopping, total=total + 1, bl=new_bl)
-
-            for my, mx, mori in moves:
-                solve(mx, my, mori, gas=gas - 10, food=food - 4, drink=drink - 6, mood=mood - 8, path=path + [mori],
-                    shopping=shopping, total=total + 1, bl=bl)
-
-        solve(5, 14, base_ori)
-
-        return best, rpath
+        best, rpath = game(maze, base_ori, ring)
+        strpath = [translate[k] for k in rpath]
+        return best, strpath
 
     @commands.command(name='solveurl')
     @commands.is_owner()
     async def _solveurl(self, ctx, *, url: str):
         image = await self.fetch_image(url)
-        partial = functools.partial(self.game, image)
-        m = await ctx.send("Solving...")
+
+        def predicate(reaction, user):
+            return str(reaction.emoji) in ('✅', '❌') and user == ctx.author
+
+        maze, base_ori, ring = self.make_map(image)
+        embed = discord.Embed(color=discord.Color.greyple())
+
+        if ring:
+            embed.description = "💍 | Ring Found, Try Solving with the Ring ?"
+            m = await ctx.reply(embed=embed)
+            await m.add_reaction('✅')
+            await m.add_reaction('❌')
+
+            try:
+                reaction, user = await ctx.bot.wait_for('reaction_add', timeout=20, check=predicate)
+
+                if str(reaction.emoji) == '❌':
+                    ring = False
+
+                embed.description = "Solving.."
+                await m.clear_reactions()
+                await m.edit(embed=embed)
+            except asyncio.TimeoutError:
+                return
+        else:
+            embed.description = "Solving.."
+            m = await ctx.reply(embed=embed)
+            ring = False
+
+        partial = functools.partial(self.game_setup, maze, base_ori, ring)
         affection, solution = await self.bot.loop.run_in_executor(None, partial)
 
         if solution:
-            await m.edit(content=f"{ctx.author.mention} | `{affection} AP` | `{', '.join(solution)}`")
+            await m.edit(content=f"`{affection} AP` | `{', '.join(solution)}`", embed=None)
         else:
-            await m.edit(content=f"No Solution Found :(")
+            await m.edit(content="No Solution Found :(", embed=None)
 
     @commands.command(name='solve')
+    @commands.max_concurrency(1, per=BucketType.user, wait=False)
     async def _solve(self, ctx):
         await ctx.send("Please do **`k!vi`**")
 
@@ -242,14 +219,47 @@ class DateSolver(commands.Cog):
 
         url = message.embeds[0].image.url
         image = await self.fetch_image(url)
-        partial = functools.partial(self.game, image)
-        m = await ctx.send("Solving...")
+
+        def pred(reaction, user):
+            return str(reaction.emoji) in ('✅', '❌') and user == ctx.author
+
+        maze, base_ori, ring = self.make_map(image)
+        embed = discord.Embed(color=discord.Color.greyple())
+
+        if ring:
+            embed.description = "💍 | Ring Found, Try Solving with the Ring ?"
+            m = await ctx.reply(embed=embed)
+            await m.add_reaction('✅')
+            await m.add_reaction('❌')
+
+            try:
+                reaction, user = await ctx.bot.wait_for('reaction_add', timeout=20, check=pred)
+
+                if str(reaction.emoji) == '❌':
+                    ring = False
+
+                embed.description = "Solving.."
+                await m.clear_reactions()
+                await m.edit(embed=embed)
+            except asyncio.TimeoutError:
+                return
+        else:
+            embed.description = "Solving.."
+            m = await ctx.reply(embed=embed)
+            ring = False
+
+        partial = functools.partial(self.game_setup, maze, base_ori, ring)
         affection, solution = await self.bot.loop.run_in_executor(None, partial)
 
         if solution:
-            await m.edit(content=f"{ctx.author.mention} | `{affection} AP` | `{', '.join(solution)}`")
+            await m.edit(content=f"`{affection} AP` | `{', '.join(solution)}`", embed=None)
         else:
-            await m.edit(content=f"No Solution Found :(")
+            await m.edit(content="No Solution Found :(", embed=None)
+
+    @_solve.error
+    async def solve_error(self, ctx, error):
+        if isinstance(error, commands.MaxConcurrencyReached):
+            await ctx.send(f"{ctx.author.mention}, an instance of this command is already running.")
 
 def setup(bot):
     bot.add_cog(DateSolver(bot))
